2bb34ed3f62dcada5bc9be18d39d4166294bfa52
NinelHdz <141677082+NinelHdz@users.noreply.github.com>
2023-08-12 09:46:05 -0600

    Update exercise.py

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    a = float(input('Calificación de la materia: '))
+    b = float(input('Calificación de la materia: '))
+    c = float(input('Calificación de la materia: '))
+    d = float(input('Calificación de la materia: '))
+    p = (a+b+c+d)/4
+    print('El promedio es: ',p)
 
 if __name__ == '__main__':
     main()
